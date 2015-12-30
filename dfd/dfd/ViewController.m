df5daa712903e45a444e622f79370ca393c54f6d
jilljiejie <qianfeng@wangjie.local>
2015-12-30 22:02:22 +0800

    jnk

---
+++ dfd/dfd/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  dfd
+//
+//  Created by qianfeng on 15/12/30.
+//  Copyright (c) 2015年 wj. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view,
+    
+//    typically from a nib.
+    
+    
+    self.view.backgroundColor=[UIColor  whiteColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
